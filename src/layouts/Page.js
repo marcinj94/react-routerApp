43dae5e262783cba4548d48bd311510b0773bee8
beardofdoom <marcin.jusiak@gmail.com>
2019-02-17 17:23:54 +0100

    Add Page component

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../styles/Page.css';
 
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import BandsPage from '../pages/BandsPage';
+import ChannelsPage from '../pages/ChannelsPage';
+import ErrorPage from '../pages/ErrorPage';
+
+
 const Page = () => {
     return (
-        <div>
-            Page
-        </div>
+        <Switch>
+            <Route path="/" exact component={HomePage}></Route>
+            <Route path="/bands" component={BandsPage}></Route>
+            <Route path="/channels" component={ChannelsPage}></Route>
+            <Route component={ErrorPage}></Route>
+        </Switch>
     );
 }
 
---
+++ src/pages/ChannelsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelsPage = () => {
+    return (
+        <div>
+            channels page
+        </div>
+    );
+}
+
+export default ChannelsPage;
